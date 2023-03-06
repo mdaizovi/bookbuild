e59175c147763f0d707b815298d8095f4aa4ad48
Mic <mdaizovi@gmail.com>
2023-03-06 13:17:50 +0100

    put category back in list display

--- bookbuild_src/importer/management/commands/export_text.py
+++ bookbuild_src/importer/management/commands/export_text.py
@@ -23,14 +23,25 @@ class Command(BaseCommand):
         document = Document()
         for n in Neighborhood.objects.all():
             document.add_heading(n.title, 0)
+            cat = OrderedDict({"None":[]})
             for b in Blob.objects.filter(neighborhood = n).order_by('category__title'):
-                document.add_heading(b.title, level=2)
-                #title.add_run('bold').bold = True
-                document.add_paragraph(b.category.title)
-                document.add_paragraph(f"Priority: {str(b.priority)}")
-                document.add_paragraph(b.main_text)
-                document.add_paragraph(b.footer_text)
-                document.add_paragraph("----")
+                if not b.category:
+                    cat["None"] = [b]
+                elif b.category.title not in cat:
+                    cat[b.category.title] = [b]
+                else:
+                    cat[b.category.title].append(b)
+
+            for c, b_list in cat.items():
+                if len(b_list) > 0:
+                    document.add_heading(c, level=3)
+                    for b in b_list:
+                        document.add_heading(b.title, level=4)
+                        #title.add_run('bold').bold = True
+                        document.add_paragraph(f"Priority: {str(b.priority)}")
+                        document.add_paragraph(b.main_text)
+                        #document.add_paragraph(b.footer_text)
+                    document.add_paragraph("----")
 
             document.add_page_break()
 
