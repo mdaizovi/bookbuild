32fede46c3b1fb199c37649dea29aa5fcdcc0662
Mic <mdaizovi@gmail.com>
2023-09-24 20:31:33 +0200

    change aws settings

--- bookbuild_src/bookbuild_src/settings.py
+++ bookbuild_src/bookbuild_src/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-
-import os
 import sys
 
 # https://django-environ.readthedocs.io/en/latest/
@@ -138,7 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = "/static/"
+# STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Reminder: edit the files in STATICFILES_DIRS. the ones in static are auto generated.
@@ -146,6 +144,9 @@ STATICFILES_DIRS = ["staticfiles"]
 
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_S3_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
+AWS_S3_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
+
 AWS_STORAGE_BUCKET_NAME = "bookbuild-ebook-assets"
 AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
@@ -155,12 +156,12 @@ MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, "media")
 DEFAULT_FILE_STORAGE = "ebook_exporter.storage_backends.MediaStorage"
 
 AWS_DEFAULT_ACL = None  # else you get access denied
-AWS_DEFAULT_ACL = "public-read"
+# AWS_DEFAULT_ACL = "public-read"
 AWS_BUCKET_ACL = "public-read"
 
-# AWS_LOCATION = 'static'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 MEDIA_ROOT = os.path.join(BASE_DIR, "media") if ENV_TYPE == ENV_DEV else None
 
 
