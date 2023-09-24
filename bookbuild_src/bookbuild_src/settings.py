dcf80d8aee91e0738e5ec012812e05788cbad48e
Mic <mdaizovi@gmail.com>
2023-09-24 20:17:01 +0200

    change bucket name

--- bookbuild_src/bookbuild_src/settings.py
+++ bookbuild_src/bookbuild_src/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = ["staticfiles"]
 
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = "bookbuild-assets"
+AWS_STORAGE_BUCKET_NAME = "bookbuild-ebook-assets"
 AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
